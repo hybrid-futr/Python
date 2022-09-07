a54addc1061f1bf3c88df88fd23ed2a45eae0e97
J.F. Ramirez <admin@hybridfutr.com>
2022-09-06 21:58:55 -0700

    Update weatherscraper.py

--- projects/weatherscraper.py
+++ projects/weatherscraper.py
@@ -1,4 +1,4 @@
-#This excercise is from: https://www.dataquest.io/blog/web-scraping-python-using-beautiful-soup/
+#This exercise is from: https://www.dataquest.io/blog/web-scraping-python-using-beautiful-soup/
 #I used LA instead of SF: https://forecast.weather.gov/MapClick.php?lat=34.0536&lon=-118.2454#.YxediC2B2Ls
 #Safari web dev tools: https://developer.apple.com/safari/tools/#current
 
